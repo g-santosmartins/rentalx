6c4c7b2e8ae3bd265ba13340611358f05f186fd0
g-santosmartins <g.santosmartins@outlook.com>
2022-01-15 23:34:48 -0300

    feat: Adicionando o useCase Create Category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,17 @@
-import { Router, Request, Response } from 'express'
+import { Router} from 'express'
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository'
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService'
-
+import { createCategoryController } from '../modules/cars/useCases/createCategory'
 
 const categoriesRoutes = Router()
 
 const categoriesRepository = new CategoriesRepository()
 
 // Route to register a new category based on the params
-categoriesRoutes.post("/", (request: Request, response: Response) => {
-  const { name, description } = request.body
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository)
-
-  createCategoryService.execute({name, description})
-
-  return response.status(201).send()
+categoriesRoutes.post("/", (request, response) => {
+  return createCategoryController.handle(request, response)
 })
 
-categoriesRoutes.get("/", (request: Request, response: Response) => {
+categoriesRoutes.get("/", (request, response) => {
   const all = categoriesRepository.list()
   // returns all categories
   return response.status(200).json(all)
