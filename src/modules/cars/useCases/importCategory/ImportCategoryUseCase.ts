535a86a9149f1d3b1d274e3c40006d76e6d60c66
g-santosmartins <g.santosmartins@outlook.com>
2022-01-16 20:38:34 -0300

    feat: Implementando o import de categorias

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,21 +1,61 @@
 import { parse } from 'csv-parse';
 import fs from 'fs'
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
+interface IImportCategory {
+  name: string,
+  description: string;
+}
 
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File) : void{
+  constructor(private categoriesRepository: ICategoriesRepository) { }
 
-    // setting up a stream of data
+  loadCategories(file: Express.Multer.File) :Promise<IImportCategory[]> {
 
-    const stream = fs.createReadStream(file.path)
-    const parseFile = parse()
+    return new Promise((resolve, reject) => {
+      // setting up a stream of data
 
-    stream.pipe(parseFile)
+      const stream = fs.createReadStream(file.path)
 
-    parseFile.on("data", async (line) => {
-      console.log(line)
+      const categories: IImportCategory[] = []
+      const parseFile = parse()
+
+      stream.pipe(parseFile)
+
+      parseFile.on("data", async (line) => {
+
+        const [name, description] = line
+
+        categories.push({
+          name,
+          description,
+        })
+      })
+      .on("end", () => {
+        resolve(categories)
+      })
+      .on("error", (err) => {
+        reject(err)
+      })
+    })
+  }
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+
+    categories.map(async category => {
+      const {name, description} = category
+
+      const existsCategory = this.categoriesRepository.findByName(name)
+
+      if(!existsCategory) {
+        this.categoriesRepository.create({ 
+          name, 
+          description
+        })
+      }
+
     })
   }
 }
 
-export {ImportCategoryUseCase}
+export { ImportCategoryUseCase }
