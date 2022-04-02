b5072bfc54bd9138b77c3d77bcc35c2d3aaa5d1c
g-santosmartins <g.santosmartins@outlook.com>
2022-04-02 13:50:10 -0300

    fix: uptade user avatar controller

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -1,6 +1,6 @@
 import { Response, Request } from 'express'
 import { container } from 'tsyringe'
-import { UpdataUserAvatarUseCase } from './UpdateUserAvatarUseCase'
+import { UpdateUserAvatarUseCase } from './UpdateUserAvatarUseCase'
 
 class UpdateUserAvatarController {
    async handle(request: Request, response: Response): Promise<Response> {
@@ -9,7 +9,7 @@ class UpdateUserAvatarController {
       // receiving the file
       const avatar_file = request.file.filename
 
-      const updateUserAvatarUseCase = container.resolve(UpdataUserAvatarUseCase)
+      const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase)
 
       await updateUserAvatarUseCase.execute({ user_id: id, avatar_file })
 
